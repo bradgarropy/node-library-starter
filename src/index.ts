82a4aaaaf1bc300897438561b89300f7cfaa4628
Brad Garropy <bradgarropy@gmail.com>
2021-02-22 22:40:13 -0600

    bad commit.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 const sayHello = (name = "world"): string => {
     const message = `Hello, ${name}!`
     console.log(message)
+    const foo = 2
 
-    return message
+    return     message;
 }
 
 export = sayHello
