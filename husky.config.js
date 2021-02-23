be865128b38481bd473f119070e0f939f999ed06
Brad Garropy <bradgarropy@gmail.com>
2021-02-22 22:43:01 -0600

    different husky command.

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,7 @@
 const config = {
     hooks: {
-        "pre-commit": "npm run lint:fix && npm run format:fix",
+        // "pre-commit": "npm run lint:fix && npm run format:fix",
+        "pre-commit": "npm run test",
     },
 }
 
